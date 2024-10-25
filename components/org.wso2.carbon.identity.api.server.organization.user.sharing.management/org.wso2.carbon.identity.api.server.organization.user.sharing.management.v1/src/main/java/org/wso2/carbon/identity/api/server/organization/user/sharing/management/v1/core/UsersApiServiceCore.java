d8976a4879370e328f34a65f0d3585ef8eb027c5
BimsaraBodaragama <bodaragamadb.office@gmail.com>
2024-10-26 01:51:06 +0530

    Implement selective share api call

--- components/org.wso2.carbon.identity.api.server.organization.user.sharing.management/org.wso2.carbon.identity.api.server.organization.user.sharing.management.v1/src/main/java/org/wso2/carbon/identity/api/server/organization/user/sharing/management/v1/core/UsersApiServiceCore.java
+++ components/org.wso2.carbon.identity.api.server.organization.user.sharing.management/org.wso2.carbon.identity.api.server.organization.user.sharing.management.v1/src/main/java/org/wso2/carbon/identity/api/server/organization/user/sharing/management/v1/core/UsersApiServiceCore.java
@@ -18,12 +18,24 @@
 
 package org.wso2.carbon.identity.api.server.organization.user.sharing.management.v1.core;
 
+import org.wso2.carbon.identity.api.server.organization.user.sharing.management.v1.model.RoleWithAudience;
 import org.wso2.carbon.identity.api.server.organization.user.sharing.management.v1.model.UserShareRequestBody;
+import org.wso2.carbon.identity.api.server.organization.user.sharing.management.v1.model.UserShareRequestBodyOrganizations;
 import org.wso2.carbon.identity.api.server.organization.user.sharing.management.v1.model.UserShareWithAllRequestBody;
 import org.wso2.carbon.identity.api.server.organization.user.sharing.management.v1.model.UserSharedOrganizationsResponse;
 import org.wso2.carbon.identity.api.server.organization.user.sharing.management.v1.model.UserSharedRolesResponse;
 import org.wso2.carbon.identity.api.server.organization.user.sharing.management.v1.model.UserUnshareRequestBody;
 import org.wso2.carbon.identity.api.server.organization.user.sharing.management.v1.model.UserUnshareWithAllRequestBody;
+import org.wso2.carbon.identity.organization.management.organization.user.sharing.UserSharingPolicyHandlerServiceImpl;
+import org.wso2.carbon.identity.organization.management.organization.user.sharing.models.AbstractUserShareDO;
+import org.wso2.carbon.identity.organization.management.organization.user.sharing.models.PolicyBearingOrganization;
+import org.wso2.carbon.identity.organization.management.organization.user.sharing.models.SharedRole;
+import org.wso2.carbon.identity.organization.management.organization.user.sharing.models.UserCriteria;
+import org.wso2.carbon.identity.organization.management.organization.user.sharing.models.UserShareGeneralDO;
+import org.wso2.carbon.identity.organization.management.organization.user.sharing.models.UserShareSelectiveDO;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Core service class for handling user sharing management APIs.
@@ -36,9 +48,50 @@ public class UsersApiServiceCore {
      * @param userShareRequestBody Contains details for user sharing.
      */
     public void shareUser(UserShareRequestBody userShareRequestBody) {
-        // Core logic for sharing the user across specific organizations
-        // Example: Call relevant service methods to share the user.
-        // Throw appropriate exceptions for error cases, e.g., validation failures.
+
+        UserSharingPolicyHandlerServiceImpl userSharingPolicyHandlerService = new UserSharingPolicyHandlerServiceImpl();
+
+        List<String> userIds = userShareRequestBody.getUserCriteria().getUserIds();
+        List<UserShareRequestBodyOrganizations> organizations = userShareRequestBody.getOrganizations();
+
+
+        for(String userId : userIds) {
+
+            for(UserShareRequestBodyOrganizations organization : organizations) {
+
+                UserShareSelectiveDO userShareSelectiveDO = new UserShareSelectiveDO();
+                userShareSelectiveDO.setUserId(userId);
+                userShareSelectiveDO.setOrganizationId(organization.getOrgId());
+
+                List<SharedRole> sharedRolesList = getSharedRoles(organization);
+
+                userShareSelectiveDO.setRoles(sharedRolesList);
+
+                try {
+                    userSharingPolicyHandlerService.propagateSelectiveShare(userShareSelectiveDO);
+                } catch (Exception e){
+                    //do something
+                }
+
+
+            }
+        }
+
+    }
+
+    private static List<SharedRole> getSharedRoles(UserShareRequestBodyOrganizations organization) {
+
+        List<SharedRole> sharedRolesList = new ArrayList<>();
+
+        List<RoleWithAudience> roles = organization.getRoles();
+        for(RoleWithAudience role : roles) {
+            SharedRole sharedRole = new SharedRole();
+            sharedRole.setRoleName(role.getDisplayName());
+            sharedRole.setAudienceName(role.getAudience().getDisplay());
+            sharedRole.setAudienceType(role.getAudience().getType());
+            sharedRolesList.add(sharedRole);
+        }
+        return sharedRolesList;
     }
 
     /**
